c584661949308a65ad118cadbecd4489c3bf19aa
gnems <g_nemsveridze@cu.edu.ge>
2019-03-23 17:57:02 +0400

    Working on LINQ

---
+++ CSharpTutA/CSharpTutA/Owner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpTutA
+{
+    class Owner
+    {
+        public string Name { get; set; }
+        public string OwnerID { get; set; }
+    }
+}
---
+++ CSharpTutA/CSharpTutA/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpTutA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryStringArray();
+
+            Console.ReadKey();
+        }
+
+        static void QueryStringArray()
+        {
+
+        }
+    }
+}
